b00a6b36528a153793a740314e741a982a41e1e0
Piyal <chakravertypiyal@gmail.com>
2021-09-15 03:20:19 +0530

    Initial commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Moxie" };
+            return View(movie);
+            //return RedirectToAction("Index", "Home");
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(string.Format("{0}/{1}", month, year));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("ID = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("Page index = {0}, Sort by = {1}", pageIndex, sortBy));
+        }
+    }
+} 
